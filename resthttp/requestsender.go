8f8a00dac6d8366e0bb134f94f8ebe4958712610
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-10 15:37:50 +0800

    Deploy getter

--- resthttp/requestsender.go
+++ resthttp/requestsender.go
@@ -2,6 +2,8 @@ package resthttp
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 )
 
@@ -52,20 +54,38 @@ func (ta tokenAuth) GetJSON() string {
 }
 
 //Making request
-func (reqI requestInfo) doRequest(a *Auth) {
+func (reqI requestInfo) doRequest(a *[]Auth) string {
 	_ = SetContentType(reqI.Content)
 	_ = SetCORS(reqI.CrossOrigin)
 	if a != nil { /* If authorization is provided */
-		var authmeta Auth = *a
-		if b, isBA := authmeta.(basicAuth); isBA {
-			b.getBAURL(reqI)
-		} else if t, isTA := authmeta.(tokenAuth); isTA {
-			t.assignTokenHeader(false)
-		} else {
-			panic("This is not a valid Auth interface")
+		for _, authmeta := range *a {
+			if b, isBA := authmeta.(basicAuth); isBA {
+				b.getBAURL(reqI)
+			} else if t, isTA := authmeta.(tokenAuth); isTA {
+				t.assignTokenHeader(false)
+			} else {
+				panic("This is not a valid Auth interface")
+			}
 		}
 	}
-	//res, err := http.Get(reqI.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", reqI.URL, nil)
+	if err != nil {
+		panic("Unable do GET request")
+	}
+	for _, h := range headerSet {
+		req.Header.Set(h.name, h.value)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		panic("No response of target website")
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic("Fail to read context of the website")
+	}
+	return string(body)
 }
 
 func (ba basicAuth) getBAURL(reqI requestInfo) {
@@ -79,4 +99,55 @@ func (ba basicAuth) getBAURL(reqI requestInfo) {
 
 func (ta tokenAuth) assignTokenHeader(allowOverRide bool) {
 	_ = SetCustomHeader(ta.headerName, ta.token, allowOverRide)
+}
+
+func getReqInfo(url, content, cors string) requestInfo {
+	return requestInfo{
+		URL:         url,
+		Content:     content,
+		CrossOrigin: cors,
+	}
+}
+
+//SendRequest : Send request with no authorize method
+func SendRequest(url, content, cors string) string {
+	i := getReqInfo(url, content, cors)
+	return i.doRequest(nil)
+}
+
+//SendRequestWithBasicAuth : Send request with username and password
+func SendRequestWithBasicAuth(url, content, cors, uname, pwd string) string {
+	ba := basicAuth{
+		username: uname,
+		password: pwd,
+	}
+	reqBa := &[]Auth{ba}
+	i := getReqInfo(url, content, cors)
+	return i.doRequest(reqBa)
+}
+
+//SendRequestWithTokenAuth : Send request with token
+func SendRequestWithTokenAuth(url, content, cors, tokenHeader, tokenStr string) string {
+	ta := tokenAuth{
+		headerName: tokenHeader,
+		token:      tokenStr,
+	}
+	reqTa := &[]Auth{ta}
+	i := getReqInfo(url, content, cors)
+	return i.doRequest(reqTa)
+}
+
+//SendRequestWithBasicAndToken : Send request with username, password and token
+func SendRequestWithBasicAndToken(url, content, cors, uname, pwd, tokenHeader, tokenStr string) string {
+	ba := basicAuth{
+		username: uname,
+		password: pwd,
+	}
+	ta := tokenAuth{
+		headerName: tokenHeader,
+		token:      tokenStr,
+	}
+	req := &[]Auth{ba, ta}
+	i := getReqInfo(url, content, cors)
+	return i.doRequest(req)
 }
