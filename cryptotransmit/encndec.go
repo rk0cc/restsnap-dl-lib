56a3de4d18119c832a7267a09b2f9f37491bea1b
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-11 20:24:25 +0800

    Working on rsa codec

--- cryptotransmit/encndec.go
+++ cryptotransmit/encndec.go
@@ -1,6 +1,31 @@
 package cryptotransmit
 
-// TODO implement actual directory
-func getPriK() string {
-	return ""
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+
+	rsmsc "github.com/rk0cc/restsnap-dl-lib/misc"
+)
+
+//Get public key
+func getPubKey() string {
+	ks, err := ioutil.ReadFile(rsmsc.GetDataPath("restsnap.key"))
+	if err != nil {
+		panic("Unable to read key from restsnap")
+	}
+	return string(ks)
+}
+
+func pubKeyParser(pubkStr string) *rsa.PublicKey {
+	blk, _ := pem.Decode([]byte(pubkStr))
+	if blk == nil {
+		panic("Failed to parse block of public key")
+	}
+	pubk, err := x509.ParsePKCS1PublicKey(blk.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	return pubk
 }
