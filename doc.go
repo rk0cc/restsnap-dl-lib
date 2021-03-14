7a62cfe24062953790f84a4fc4e81ffc2f035be9
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-14 20:30:21 +0800

    Add documents

---
+++ doc.go
@@ -0,0 +1,7 @@
+//Package restsnap_dl_lib
+//
+//The library for RestSnap
+//
+//This is an internal package for RestSnap only,
+//deploying in any non-restsnap or forked project is forbidden
+package restsnap_dl_lib
---
+++ resthttp/doc.go
@@ -0,0 +1,4 @@
+//Package resthttp is a HTTP handler for making request and return response data
+//
+//All data should not parse to database directly, and must be generated as signature at first
+package resthttp
