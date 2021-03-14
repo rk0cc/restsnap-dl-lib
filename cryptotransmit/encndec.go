f17ce0caede71a49b9726a95cdf5c11e5c499831
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-14 20:14:14 +0800

    Complete sign and verify

--- cryptotransmit/encndec.go
+++ cryptotransmit/encndec.go
@@ -1,21 +1,54 @@
 package cryptotransmit
 
 import (
+	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
 	"io/ioutil"
 
 	rsmsc "github.com/rk0cc/restsnap-dl-lib/misc"
+	"golang.org/x/crypto/sha3"
 )
 
-//Get public key
-func getPubKey() string {
-	ks, err := ioutil.ReadFile(rsmsc.GetDataPath("restsnap.key"))
+//RSKeySet is a type allows JSON formatted to store unique data for crypto
+//
+//All data must be generated from Java
+type RSKeySet struct {
+	PrivateKey string `json:"private_key"` //Private key
+	Append     string `json:"append"`      //String append
+}
+
+func getRSKey() RSKeySet {
+	rsks := &RSKeySet{}
+	rsksStr, err := ioutil.ReadFile(rsmsc.GetDataPath("restsnap.key"))
 	if err != nil {
-		panic("Unable to read key from restsnap")
+		panic("Unable to read key set from restsnap")
 	}
-	return string(ks)
+	err = json.Unmarshal(rsksStr, rsks)
+	if err != nil {
+		panic("This may not RSKeySet format, may be modified manually")
+	}
+	return *rsks
+}
+
+//Get private key
+func getPriKey() string {
+	return string(getRSKey().PrivateKey)
+}
+
+func priKeyParser(prikStr string) *rsa.PrivateKey {
+	blk, _ := pem.Decode([]byte(prikStr))
+	if blk == nil {
+		panic("Failed to parse block of private key")
+	}
+	prik, err := x509.ParsePKCS1PrivateKey(blk.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	return prik
 }
 
 func pubKeyParser(pubkStr string) *rsa.PublicKey {
@@ -28,4 +61,35 @@ func pubKeyParser(pubkStr string) *rsa.PublicKey {
 		panic(err)
 	}
 	return pubk
+}
+
+//GenDataSign : Generate data signature for varify
+//Note: This will return JSON formatted byte array
+func GenDataSign(data []byte) []byte {
+	hashedData := sha3.New256()
+	if _, err := hashedData.Write(data); err != nil {
+		panic(err)
+	}
+	sum := hashedData.Sum([]byte(getRSKey().Append))
+	sign, err := rsa.SignPKCS1v15(rand.Reader, priKeyParser(getPriKey()), crypto.SHA3_256, sum)
+	if err != nil {
+		panic("Create new signature failed")
+	}
+	marsedData, _ := json.Marshal(struct {
+		Sign    string `json:"sign"`
+		HashSum string `json:"hashsum"`
+	}{
+		Sign:    string(sign),
+		HashSum: string(sum),
+	})
+	return marsedData
+}
+
+//VerifyData : Verify signature that is generated before
+func VerifyData(hashSum, sign []byte, publicKey string) bool {
+	failed := rsa.VerifyPKCS1v15(pubKeyParser(publicKey), crypto.SHA3_256, hashSum, sign)
+	if failed != nil {
+		return false
+	}
+	return true
 }
