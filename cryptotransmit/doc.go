06e2c2e14edf7107d2411999033506de5500b52f
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-14 20:23:45 +0800

    Add document

---
+++ cryptotransmit/doc.go
@@ -0,0 +1,7 @@
+//Package cryptotransmit is a verfication handler of Rest Snap
+//
+//The aim of this package is to ensure the data must be get and use from the same computer
+//since the API data may be updated
+//
+//The data will be convert as signature after HTTP respond, and will wait to verify if needed
+package cryptotransmit
